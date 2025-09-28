e7059a9cb5eb6d37d935ecddb2849271a2f3e716
Simon Raynor <ssraynor@googlemail.com>
2025-09-28 19:51:34 +0100

    WIP advanced tiles

--- src/advanced/index.ts
+++ src/advanced/index.ts
@@ -1,10 +1,12 @@
 
-const DEFAULT_SIZE = 96;
-const DEFAULT_LINESPER = 3;
+const DEFAULT_SIZE = 64;
+const DEFAULT_LINESPER = 2 + Math.floor(Math.random() * 2);
 
-const LINE_WIDTH = 7;
+const DEFAULT_SPACE = 1 - (1 / DEFAULT_LINESPER);
 
-const NUMBER_OF_TYPES = 3;
+const LINE_WIDTH = 6 + Math.floor(Math.random() * 6);
+
+const NUMBER_OF_TYPES = 2 + Math.floor(Math.random() * 3);
 
 
 type Point = [number, number];
@@ -16,14 +18,12 @@ class Tile {
     numEdges: number = 4
     corners: Point[] = [[-1, -1], [1, -1], [1, 1], [-1, 1]]
     linesPerEdge: number
-    edges: number[]
     lines: Line[]
 
     constructor(
         linesPerEdge = DEFAULT_LINESPER
     ) {
         this.linesPerEdge = linesPerEdge;
-        this.edges = [];
         this.lines = [];
 
         const lineCount = this.linesPerEdge * this.numEdges;
@@ -39,12 +39,18 @@ class Tile {
                 1
             );
 
-            const other = spotsAvailable[
-                    (1 + Math.floor(Math.random() * (spotsAvailable.length - 2))) % spotsAvailable.length
+            const thisEdgeNo = this.edgeNo(idx);
+
+            const available = spotsAvailable.filter(other => {
+                const edgeNo = this.edgeNo(other);
+
+                return edgeNo !== thisEdgeNo
+                    && edgeNo !== thisEdgeNo + (this.numEdges / 2);
+            });
+
+            const other = available[
+                    Math.floor(Math.random() * available.length) % available.length
                 ];
-            
-            /* const edge1No = Math.floor(idx / this.linesPerEdge);
-            const edge2No = Math.floor(other / this.linesPerEdge); */
 
             spotsAvailable.splice(
                 spotsAvailable.indexOf(other),
@@ -52,23 +58,71 @@ class Tile {
             );
 
             spotsTaken.push(idx, other);
+            
+            const [x0, y0] = this.edgePointCoord(idx);
+            const [x1, y1] = this.edgePointCoord(other);
 
-            this.edges[idx] = other;
-            this.edges[other] = idx;
+            const xdiff = Math.abs(x0 - x1);
+            const ydiff = Math.abs(y0 - y1);
+
+            const [[sx0, sy0], [ex0, ey0]] = this.edgeEnds(idx);
+            const [[sx1, sy1], [ex1, ey1]] = this.edgeEnds(other);
+
+            let sharedCorner: Point | null = null;
+
+            if (sx0 === ex1 && sy0 === ey1) sharedCorner = [sx0, sy0];
+            if (sx1 === ex0 && sy1 === ey0) sharedCorner = [sx1, sy1];
+
+            if (sharedCorner) {
+                if (xdiff < 2 * ydiff && ydiff < 2 * xdiff) {
+                    this.lines.push(
+                        new ArcLine(
+                            this.edgePointCoord(idx),
+                            this.edgePointCoord(other),
+                            this.scaleCoords(sharedCorner)
+                        )
+                    );
+                } else {
+                    const [cx, cy] = this.scaleCoords(sharedCorner);
+
+                    const dx = cx - x0;
+                    const dy = cy - y0;
+
+                    const point: Point = [x1 - dx, y1 - dy];
+
+                    this.lines.push(
+                        new DotLine(
+                            this.edgePointCoord(idx),
+                            this.edgePointCoord(other),
+                            point
+                        )
+                    );
+                }
+            }/*  else {
+                this.lines.push(
+                    new Line(
+                        this.edgePointCoord(idx),
+                        this.edgePointCoord(other)
+                    )
+                );
+            } */
 
 
-            this.lines.push(
-                new Line(
-                    this.edgePointCoord(idx),
-                    this.edgePointCoord(other),
-                    [this.size, this.size]
-                )
-            )
         }
     }
 
+
+    scaleCoords([x, y]: Point): Point {
+        return [this.size + (x * this.size), this.size + (y * this.size)];
+    }
+
+
+    edgeNo(idx: number): number {
+        return Math.floor(idx / this.linesPerEdge);
+    }
+
     edgeEnds(idx: number): [Point, Point] {
-        const edgeNo = Math.floor(idx / this.linesPerEdge);
+        const edgeNo = this.edgeNo(idx);
         const nextEdge = (edgeNo + 1) % this.numEdges;
 
         const a = this.corners[edgeNo];
@@ -79,14 +133,24 @@ class Tile {
 
     edgePointCoord(idx: number): Point {
         const pointNo =  idx % this.linesPerEdge;
-        const ratio = (1 + pointNo) / (1 + this.linesPerEdge);
+        const ratio = pointNo / (this.linesPerEdge - 1);
+
+        const space = DEFAULT_SPACE;
+        const pad = 1 - space;
+        const paddedRatio = (pad/2) + (space * ratio);
 
         const [[x0, y0], [x1, y1]] = this.edgeEnds(idx);
 
-        const x = x0 + (ratio * (x1 - x0));
-        const y = y0 + (ratio * (y1 - y0));
+        const x = x0 + (paddedRatio * (x1 - x0));
+        const y = y0 + (paddedRatio * (y1 - y0));
 
-        return [this.size + (x * this.size), this.size + (y * this.size)];
+        return this.scaleCoords([x, y]);
+    }
+
+    edgePointNormal(idx: number): Point {
+        const [[x0, y0], [x1, y1]] = this.edgeEnds(idx);
+
+        return [-(y1 - y0), (x1 - x0)];
     }
 }
 
@@ -94,15 +158,36 @@ class Tile {
 class Line {
     from: Point
     to: Point
-    via: Point
 
+    constructor(
+        from: Point,
+        to: Point,
+    ) {
+        this.from = from;
+        this.to = to;
+    }
+
+    toString() {
+        const [x0, y0] = this.from;
+        const [x2, y2] = this.to;
+
+        return `M${x0} ${y0} L${x2} ${y2}`;
+    }
+
+    embellish(ctx: CanvasRenderingContext2D) {
+
+    }
+}
+
+class ArcLine extends Line {
+    via: Point
+    
     constructor(
         from: Point,
         to: Point,
         via: Point
     ) {
-        this.from = from;
-        this.to = to;
+        super(from, to);
         this.via = via;
     }
 
@@ -110,9 +195,49 @@ class Line {
         const [x0, y0] = this.from;
         const [x1, y1] = this.via;
         const [x2, y2] = this.to;
-        return `M${x0} ${y0} L${x1} ${y1} L${x2} ${y2}`;
+
+        const xrad = x0 - x2;
+        const yrad = y0 - y2;
+
+        const sweep = y0 || (xrad < 0)? 0 : 1;
+
+        // M${x0} ${y0} h10h-20h10v10v-20v10 
+        return `M${x0} ${y0} A${Math.abs(xrad)} ${Math.abs(yrad)} 0 0 ${sweep} ${x2} ${y2}`;
     }
 }
+
+class DotLine extends Line {
+    via: Point
+    
+    constructor(
+        from: Point,
+        to: Point,
+        via: Point
+    ) {
+        super(from, to);
+        this.via = via;
+    }
+
+    toString() {
+        const [x0, y0] = this.from;
+        const [x1, y1] = this.via;
+        const [x2, y2] = this.to;
+
+        // M${x0} ${y0} h10h-20h10v10v-20v10 
+        return `M${x0} ${y0} L${x1} ${y1} L${x2} ${y2}
+                M${x1 + LINE_WIDTH} ${y1}
+                    A${LINE_WIDTH} ${LINE_WIDTH} 0 1 0 ${x1 - LINE_WIDTH} ${y1}
+                    A${LINE_WIDTH} ${LINE_WIDTH} 0 1 0 ${x1 + LINE_WIDTH} ${y1}`;
+    }
+
+    embellish(ctx: CanvasRenderingContext2D) {
+        ctx.beginPath();
+        ctx.arc(this.via[0], this.via[1], LINE_WIDTH/2, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+
 
 
 class TileRenderer {
@@ -162,7 +287,7 @@ class TileRenderer {
         ); */
         tile.lines.forEach(
             (line, idx) => {
-                this.renderLine(tile, line)
+                this.renderLine(tile, line);
             }
         );
     }
@@ -181,30 +306,8 @@ class TileRenderer {
         ctx.lineWidth = LINE_WIDTH;
         ctx.strokeStyle = 'white';
         ctx.stroke(path);
-    }
 
-    renderEdge(
-        tile: Tile,
-        fromIdx: number,
-        toIdx: number
-    ) {
-        const ctx = this.#ctx;
-
-        const [x0, y0] = tile.edgePointCoord(fromIdx);
-
-        const [x1, y1] = tile.edgePointCoord(toIdx);
-
-        const path = new Path2D(`
-            M${x0} ${y0}
-            L${x1} ${y1}
-        `);
-        ctx.lineWidth = LINE_WIDTH * 2;
-        ctx.strokeStyle = 'black';
-        ctx.stroke(path);
-
-        ctx.lineWidth = LINE_WIDTH;
-        ctx.strokeStyle = 'white';
-        ctx.stroke(path);
+        line.embellish(ctx);
     }
 
     get image() {
